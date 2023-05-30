323685628c2f24e03141fecf44026ebc26a17cae
Jen <107281506+jenvdr@users.noreply.github.com>
2023-05-30 14:27:18 +0200

    refactor: remove commented out code

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -11,9 +11,6 @@ export default function Events() {
 export async function loader() {
   const response = await fetch('http://localhost:8080/events');
     if (!response.ok) {
-        // throw new Response(JSON.stringify({ message: "Could not fetch events.", }), {
-        //     status: 500,
-        // });
         throw json(
             { message: "Could not fetch events.", },
             { status: 500 }
