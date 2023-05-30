2b1ea8b146b5066c06942b0c902e6a2f66d77dd7
Jen <107281506+jenvdr@users.noreply.github.com>
2023-05-30 11:36:41 +0200

    feat: make use of backend api to fetch data from and set error and loading states

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,34 +1,34 @@
-import { Link } from "react-router-dom"
-
-const DUMMY_EVENTS = [
-    {
-        id: 'e1',
-        title: 'First event'
-    },
-    {
-        id: 'e2',
-        title: 'Second event'
-    },
-    {
-        id: 'e3',
-        title: 'Third event'
-    }
-]
+import { useEffect, useState } from "react";
+import EventsList from "../components/EventsList/EventsList";
 
 export default function Events() {
+    const [isLoading, setIsLoading] = useState(false);
+    const [fetchedEvents, setFetchedEvents] = useState();
+    const [error, setError] = useState();
+  
+    useEffect(() => {
+      async function fetchEvents() {
+        setIsLoading(true);
+        const response = await fetch('http://localhost:8080/events');
+  
+        if (!response.ok) {
+          setError('Fetching events failed.');
+        } else {
+          const resData = await response.json();
+          setFetchedEvents(resData.events);
+        }
+        setIsLoading(false);
+      }
+  
+      fetchEvents();
+    }, []);
     return (
-        <>
-            <h1>Events</h1>
-            <ul>
-                {
-                DUMMY_EVENTS.map(
-                    event => (
-                        <li key={event.id}>
-                            <Link to={event.id} >{event.title}</Link>
-                        </li>
-                    )
-                )}
-            </ul>
-        </>
-    )
+      <>
+        <div style={{ textAlign: 'center' }}>
+          {isLoading && <p>Loading...</p>}
+          {error && <p>{error}</p>}
+        </div>
+        {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
+      </>
+    );
 };
