aca073d9df1a07ff4a1ef086984641e720697fa4
Jen <107281506+jenvdr@users.noreply.github.com>
2023-05-30 17:07:38 +0200

    feat: deferr data fetching

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,21 +1,34 @@
-import { json, useLoaderData } from "react-router";
+import { defer, json, useLoaderData, Await } from "react-router";
 import EventsList from "../components/EventsList/EventsList";
+import { Suspense } from "react";
 
 export default function Events() {
-    const data = useLoaderData();
-
-    const events = data.events;
-    return <EventsList events={events} />;
+    const { events } = useLoaderData();
+    
+    return (
+        <Suspense fallback={<p className="text-center">Loading...</p>}>
+            <Await resolve={events}>
+                {(loadedEvents) => <EventsList events={loadedEvents} />}
+            </Await>
+        </Suspense>
+    );
 };
 
-export async function loader() {
-  const response = await fetch('http://localhost:8080/events');
+async function loadEvents() {
+    const response = await fetch('http://localhost:8080/events');
     if (!response.ok) {
         throw json(
             { message: "Could not fetch events.", },
             { status: 500 }
         );
     } else {
-        return response;
+        const resData = await response.json();
+        return resData.events;
     }
+}
+
+export function loader() {
+    return defer({
+        events: loadEvents(),
+    })
 }
