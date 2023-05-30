50c1c2710d54de5283574557864615bd773b6e3f
Jen <107281506+jenvdr@users.noreply.github.com>
2023-05-30 10:58:10 +0200

    feat: add list of events and display event id inside single event page

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,5 +1,12 @@
+import { useParams } from "react-router"
+
 export default function EventDetail() {
+    const params = useParams();
     return (
-        <h1>EventDetail</h1>
+        <>
+            <h1>EventDetail</h1>
+            <p>Event id: {params.eventId}</p>
+        </>
+
     )
 };
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,5 +1,34 @@
+import { Link } from "react-router-dom"
+
+const DUMMY_EVENTS = [
+    {
+        id: 'e1',
+        title: 'First event'
+    },
+    {
+        id: 'e2',
+        title: 'Second event'
+    },
+    {
+        id: 'e3',
+        title: 'Third event'
+    }
+]
+
 export default function Events() {
     return (
-        <h1>Events</h1>
+        <>
+            <h1>Events</h1>
+            <ul>
+                {
+                DUMMY_EVENTS.map(
+                    event => (
+                        <li key={event.id}>
+                            <Link to={event.id} >{event.title}</Link>
+                        </li>
+                    )
+                )}
+            </ul>
+        </>
     )
 };
